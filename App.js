c90434d8d0d2388bf597fee3d97acba2da383f54
adhirajgupta <adhirajgupta2007@gmail.com>
2022-11-06 00:14:44 +0530

    stage1

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, DefaultTheme, configureFonts } from 'react-native-paper';
+import StackAuth from './components/stackAuth';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+	const theme = {
+		...DefaultTheme,
+		roundness: 2,
+		colors: {
+			...DefaultTheme.colors,
+			primary: '#3498db',
+			accent: '#f1c40f',
+		}
+	};
+
+	return (
+		<PaperProvider theme={theme}>
+			<NavigationContainer>
+				<StackAuth />
+			</NavigationContainer>
+			<StatusBar style='auto' />
+		</PaperProvider>
+
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWBsP2EKH-8nUOUIzZvfgVUcvNPCbm3Tc",
+  authDomain: "c76-homework.firebaseapp.com",
+  projectId: "c76-homework",
+  storageBucket: "c76-homework.appspot.com",
+  messagingSenderId: "1052046452368",
+  appId: "1:1052046452368:web:975c019d21ad5935619a15"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
